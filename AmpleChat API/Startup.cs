5ff0b3e94c00f14c1143cb8450b19b47ee728a18
Boris <Borismulder@outlook.com>
2021-10-17 22:28:01 +0200

    Update Startup.cs
    Cors & db setup

--- AmpleChat API/Startup.cs
+++ AmpleChat API/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AmpleChat_API.Database;
 
 namespace AmpleChat_API
 {
@@ -23,9 +18,21 @@ namespace AmpleChat_API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+#if DEBUG
+            services.AddCors(cors => {
+                cors.AddPolicy("crs", policy => {
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyOrigin();
+                });
+            });
+#endif
+
+            services.AddDbContext<DatabaseService>(config => {
+                config.UseSqlServer(Configuration["ConnectionStrings:DEV"]);
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -34,7 +41,6 @@ namespace AmpleChat_API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,6 +49,11 @@ namespace AmpleChat_API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AmpleChat_API v1"));
             }
+
+#if DEBUG
+            app.UseCors("crs");
+#endif
+
 
             app.UseHttpsRedirection();
 
