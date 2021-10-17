fa72b00c48169a7bf98724c86dc5d60cd67ba008
Boris <Borismulder@outlook.com>
2021-10-17 22:27:06 +0200

    Create DatabaseService.cs

---
+++ AmpleChat API/Database/DatabaseService.cs
@@ -0,0 +1,47 @@
+﻿using AmpleChat_API.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmpleChat_API.Database
+{
+    public class DatabaseService : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<ContactRequest> ContactRequests { get; set; }
+
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+
+        public DatabaseService(DbContextOptions<DatabaseService> options)
+        : base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // One to many, user & contacts
+            builder.Entity<User>()
+                .HasMany(i => i.Contacts)
+                .WithOne()
+                .OnDelete(DeleteBehavior.NoAction);
+
+
+            // One to many, user & contactrequest
+            builder.Entity<User>()
+                .HasMany(i => i.ContactRequests)
+                .WithOne(i => i.User)
+                .HasForeignKey(i => i.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            // One to many, user & message
+            builder.Entity<User>()
+                .HasMany(i => i.Messages)
+                .WithOne(i => i.User)
+                .HasForeignKey(i => i.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
