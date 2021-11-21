91a4284e9babe71d045950864b0cd3afa3fbb7c3
Boris <Borismulder@outlook.com>
2021-11-21 22:05:23 +0100

    Update Account.cs

--- AmpleChat API/Database/Models/Account.cs
+++ AmpleChat API/Database/Models/Account.cs
@@ -10,8 +10,8 @@ namespace AmpleChat_API.Database.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public IList<Account> Contacts { get; set; } = new List<Account>();
-        public IList<ContactRequest> ContactRequests { get; set; } = new List<ContactRequest>();
-        public IList<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
+        //public IList<ContactRequest> ContactRequests { get; set; } = new List<ContactRequest>();
+        //public IList<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
     }
 }
 
